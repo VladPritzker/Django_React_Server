2da6b427ba7836072ebddcd96f4df6a2faf96c1b
vlad <pritzkervlad@gmail.com>
2024-04-28 10:23:02 -0400

    Login

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from myapp.views import users  # make sure to import the combined function
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('users/', users, name='users'),  # Updated to use the combined 'users' function
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]

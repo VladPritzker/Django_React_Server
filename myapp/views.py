827f6c99ebcf394a577d1b8c7c72959f8a79adf7
vlad <pritzkervlad@gmail.com>
2024-05-02 08:33:42 -0400

    Investin List

--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,11 @@ from django.contrib.auth import authenticate
 import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import FinancialRecord, User
+from .models import FinancialRecord, User, InvestingRecord
 from datetime import datetime
+
+
+
 
 @csrf_exempt
 def users(request):
@@ -136,5 +139,59 @@ def usersData(request, user_id=None):
             users = User.objects.all()
             users_data = [{'id': user.id, 'username': user.username, 'email': user.email} for user in users]
             return JsonResponse(users_data, safe=False)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
+    
+
+    
+@csrf_exempt
+def investing_records(request):
+    if request.method == 'POST':
+        try:
+            data = request.POST  # Assuming data is sent as form data
+            user_id = data.get('user_id')
+            title = data.get('title')
+            amount = data.get('amount')
+            record_date = data.get('record_date')
+
+            # Parse the date string into a datetime object
+            parsed_date = datetime.strptime(record_date, '%Y-%m-%d').date()
+
+            # Create or retrieve the user object
+            user, _ = User.objects.get_or_create(id=user_id)
+
+            # Create the investing record
+            record = InvestingRecord.objects.create(
+                user=user,
+                title=title,
+                amount=amount,
+                record_date=parsed_date
+            )
+            return JsonResponse({
+                'id': record.id,
+                'user_id': record.user.id,
+                'title': record.title,
+                'amount': str(record.amount),
+                'record_date': record.record_date.isoformat()
+            }, status=201)
+
+        except ValueError as ve:
+            return JsonResponse({'error': 'Date format error: ' + str(ve)}, status=400)
+        except Exception as e:
+            return JsonResponse({'error': 'Server error: ' + str(e)}, status=500)
+
+    elif request.method == 'GET':
+        user_id = request.GET.get('user_id')
+        records = InvestingRecord.objects.all()
+        if user_id:
+            records = records.filter(user_id=user_id)
+
+        records_data = [
+            {'id': record.id, 'user_id': record.user.id, 'record_date': record.record_date.isoformat(),
+             'title': record.title, 'amount': float(record.amount)}
+            for record in records
+        ]
+        return JsonResponse(records_data, safe=False)
+
     else:
         return JsonResponse({'error': 'Method not allowed'}, status=405)

1628ecf4f2d47b3fee301a0f06f75d2b14629085
vlad <pritzkervlad@gmail.com>
2024-05-18 10:50:50 -0400

    income data

--- myapp/models.py
+++ myapp/models.py
@@ -33,6 +33,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     money_invested = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     money_spent = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    income_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     balance_goal = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     spent_by_week = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     spent_by_month = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)

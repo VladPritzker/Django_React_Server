f640383ab8faab65dafc2106f667dcb2cc975e75
VladPritzker <94125540+VladPritzker@users.noreply.github.com>
2024-08-27 14:59:41 -0400

    test

--- myproject/urls.py
+++ myproject/urls.py
@@ -9,6 +9,7 @@ from myapp.views import (
     sleep_logs, home_views, stock_data, customCashFlowInvestment_views,
     stock_data_pdf, docusign_views
 )
+import download_pdf
 
 urlpatterns = [
     path('', home_views.homepage, name='homepage'),
@@ -45,6 +46,8 @@ urlpatterns = [
      # DocuSign Endpoints
     path('docusign/webhook/', docusign_views.docusign_webhook, name='docusign_webhook'),
     path('download-envelope-pdf/', docusign_views.download_envelope_pdf, name='download-envelope-pdf'),
+    path('download_pdf/<str:envelope_id>/', download_pdf, name='download_pdf'),
+
 
 
      

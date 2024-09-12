83641c01bab401506af869bad8e6436f0a4a693b
VladPritzker <94125540+VladPritzker@users.noreply.github.com>
2024-09-12 11:55:50 -0400

    test

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,9 +7,9 @@ from myapp.views import (
     user_views, income_views, meeting_views, financial_views,
     investing_views, note_views, expense_views, contact_views,
     sleep_logs, home_views, stock_data, customCashFlowInvestment_views,
-    stock_data_pdf, docusign_views, 
+    stock_data_pdf, 
 )
-from myapp.views.download_pdf import download_pdf
+
 
 
 urlpatterns = [
@@ -43,11 +43,6 @@ urlpatterns = [
     path('api/stock-data/', stock_data.stock_data_view, name='stock_data_view'),
     path('fetch-stock-data/', stock_data_pdf.fetch_stock_data, name='fetch_stock_data'),
     path('generate-pdf/', stock_data_pdf.generate_pdf, name='generate_pdf'),
-
-     # DocuSign Endpoints
-    path('docusign/webhook/', docusign_views.docusign_webhook, name='docusign_webhook'),
-    path('download_pdf/<str:envelope_id>/', download_pdf, name='download_pdf'),
-    
 
  
     # Password reset URLs

564b7a35e6af9fd0463efcf1cc2691cf9e32388c
VladPritzker <94125540+VladPritzker@users.noreply.github.com>
2025-03-08 07:52:04 -0500

    test

--- manage.py
+++ manage.py
@@ -2,15 +2,11 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from django.test import TestCase
-from django.urls import reverse
-from myapp.models import CustomUser
-
 import pymysql
+
 pymysql.install_as_MySQLdb()
 
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -22,16 +18,5 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
     main()
-
-
-class SimpleTestCase(TestCase):
-    def test_homepage_status_code(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_create_user(self):
-        user = CustomUser.objects.create_user(email="test@example.com", password="Test123!")
-        self.assertEqual(user.email, "test@example.com")
--- myapp/tests.py
+++ myapp/tests.py
@@ -1,6 +1,14 @@
 # myapp/tests.py
+
 from django.test import TestCase
+from django.urls import reverse
+from myapp.models import CustomUser
 
 class SimpleTestCase(TestCase):
-    def test_example(self):
-        self.assertEqual(1, 1)
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user(self):
+        user = CustomUser.objects.create_user(email="test@example.com", password="Test123!")
+        self.assertEqual(user.email, "test@example.com")

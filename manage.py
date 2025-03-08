e817e1b4034de678ffd72488ff2ba25db4888449
VladPritzker <94125540+VladPritzker@users.noreply.github.com>
2025-03-08 07:50:08 -0500

    test

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.test import TestCase
+from django.urls import reverse
+from myapp.models import CustomUser
 
 import pymysql
 pymysql.install_as_MySQLdb()
@@ -22,3 +25,13 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+class SimpleTestCase(TestCase):
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user(self):
+        user = CustomUser.objects.create_user(email="test@example.com", password="Test123!")
+        self.assertEqual(user.email, "test@example.com")
